87039c23368f9cf177072a2887d665eb64901401
Gor <gororoik1@mail.ru>
2023-02-14 00:11:45 +0400

    adaptived some parts

--- js/script.js
+++ js/script.js
@@ -36,11 +36,31 @@
 
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel();
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link')
+    toggleSlide('.catalog-item__back')
+
+
 });
 
 $('.owl-carousel').owlCarousel({
     loop:true,
-    // margin:100,
     nav:true,
     items:1,
     dots:false,
@@ -48,16 +68,16 @@ $('.owl-carousel').owlCarousel({
     navText: ["<div class='carousel-button owl-prev'><img src='icons/arrows/left.png'></div>",
     "<div class='carousel-button owl-next'><img src='icons/arrows/right.png'></div>"],
     responsiveClass:true,
-    responsive:{
-        0:{
-            nav:true,
-        },
-        800:{
-            nav:false,
-        },
-        1000:{
-            nav:true,
-            loop:true
-        }
-    }
+    // responsive:{
+    //     0:{
+    //         nav:true,
+    //     },
+    //     800:{
+    //         nav:false,
+    //     },
+    //     1000:{
+    //         nav:true,
+    //         loop:true
+    //     }
+    // }
 })
